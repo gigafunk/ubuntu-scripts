e4de782350b1b67bf43dd244f97fecd7dc8084e9
gigafunk <trevor.c.rogers@gmail.com>
2019-03-10 01:34:07 -0800

    Update desktop-display-install.sh

--- desktop/desktop-display-install.sh
+++ desktop/desktop-display-install.sh
@@ -2,9 +2,9 @@
 # Desktop interaction display
 
 # desktop session environment
-sudo wget --header='Accept-Encoding:none' -O /etc/profile.d/desktop-session.sh https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/desktop/desktop-session.sh
+sudo wget --header='Accept-Encoding:none' -O /etc/profile.d/desktop-session.sh https://raw.githubusercontent.com/gigafunk/ubuntu-scripts/master/desktop/desktop-session.sh
 sudo chmod +x /etc/profile.d/desktop-session.sh
 
 # main script
-sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/desktop-display https://raw.githubusercontent.com/NicolasBernaerts/ubuntu-scripts/master/desktop/desktop-display
+sudo wget --header='Accept-Encoding:none' -O /usr/local/sbin/desktop-display https://raw.githubusercontent.com/gigafunk/ubuntu-scripts/master/desktop/desktop-display
 sudo chmod +x /usr/local/sbin/desktop-display
